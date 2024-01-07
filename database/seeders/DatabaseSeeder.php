e3691a82466b87cba052769b44c7f785c0c53049
reiken <ultee529@outlook.jp>
2024-01-07 18:41:38 +0900

    CardをDBではなくファイルに移行、データ構造も変更

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Models\Card;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +13,5 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(CardSeeder::class);
     }
 }
